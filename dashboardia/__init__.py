761d7f09578156e6d4ec09c1681688720419b244
joeflack4 <joeflack4@gmail.com>
2017-09-05 22:47:57 -0400

    Fiddling with deployment. WIP.

--- dashboardia/__init__.py
+++ dashboardia/__init__.py
@@ -11,3 +11,7 @@ app = Flask(__name__)
 app.config.from_object(config[getenv('APP_SETTINGS', 'default')])
 app.static_folder = getenv('APP_STATIC_DIR', '../../client/assets')
 app.register_blueprint(client)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=False)
